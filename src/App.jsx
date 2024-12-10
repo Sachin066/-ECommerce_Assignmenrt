dcceb37cb4b0f247442ac9be34179e95b767710f
sachin saroj <91935812+Sachin066@users.noreply.github.com>
2024-12-10 22:23:42 +0530

    api work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
-import { useState } from 'react'
+import React, { useContext } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import context
+import UserContext from './context/UserContext';
+
+// import components
+import Header from './components/Header';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import Login from './components/Login';
+
+import ProductDetails from './components/ProductDetails';
+import Profile from './components/Profile';
+
+// redux setup
+import { Provider } from 'react-redux';
+import Store from './redux/Store';
 
 
-function App() {
+import Checkout from './components/Checkout';
 
+export default function App() {
+
+  
   return (
-    <div>sachin</div>
+
+      <div>
+
+
+        <UserContext>
+          <Provider store={Store}>
+
+            <BrowserRouter>
+
+              <div>
+                {/* <h1 className="font-bold text-xl">App is working</h1> */}
+                <Header />
+              </div>
+
+
+
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/productdetails/:id' element={<ProductDetails />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/checkout' element={<Checkout />} />
+                <Route path='/profile' element={<Profile />} />
+                
+
+              </Routes>
+            </BrowserRouter>
+          </Provider>
+        </UserContext>
+      </div>
   )
 }
-
-export default App
